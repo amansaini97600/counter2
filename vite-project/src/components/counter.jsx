faaacf8f82c8ed7d82ede3f098d353f4fe302921
amansaini97600 <amanks97600@gmail.com>
2025-06-02 20:10:08 +0530

    add fast increase and decrease count

--- vite-project/src/components/counter.jsx
+++ vite-project/src/components/counter.jsx
@@ -1,14 +1,40 @@
-import { use, useState } from 'react';
+import { use, useRef, useState } from 'react';
 import useSound from '../hooks/useSound'
 
 
 function Counter() {
     const [count, setCount] = useState(0);
     const [error, setError] = useState("");
+    const interveRef = useRef(null);
     const playSound = useSound();
+// this function for fast increase and decrease count
+const handleMouseDown = (opretion) => {
+    if(interveRef.current) return;
 
-    const increase = () => {
-        if (count < 200) {
+    interveRef.current = setInterval(() => {
+        setCount((prev) => {
+            let newValue = opretion === "add" ? prev + 1 : prev - 1;
+            if(newValue >=200 || newValue <=0){
+                clearInterval(interveRef.current);
+                interveRef.current = null;
+                return opretion === "add" ? 200 : 0; 
+            }
+            return newValue;
+        })
+    },100);
+}
+
+// this function for stop fast increase and decrease
+
+const handleMouseUp = () => {
+    clearInterval(interveRef.current);
+    interveRef.current = null;
+}
+
+
+// this function for increase count
+const increase = () => {
+    if (count < 200) {
             setCount(count + 1);
             setError("");
             playSound("increase");
@@ -17,6 +43,7 @@ function Counter() {
         }
     }
     
+    // this function for decrease count
     const decrease = () => {
         if (count > 0) {
             setCount(count - 1);
@@ -27,6 +54,7 @@ function Counter() {
         }
     }
     
+    // this function for reset count
     const reset = () => {
         setError("")
         setCount(0)
@@ -43,8 +71,11 @@ function Counter() {
                 <div>
                     <button
                         onClick={decrease}
-                        disabled={count <= 0}>
-                        Decrease-
+                        disabled={count <= 0}
+                        onMouseDown={() => handleMouseDown("sub")}
+                        onMouseLeave={handleMouseUp}
+                        onMouseUp={handleMouseUp}>
+                        Decrease(-)
                     </button>
                     <button
                         onClick={reset}
@@ -53,8 +84,11 @@ function Counter() {
                     </button>
                     <button
                         onClick={increase}
-                        disabled={count >= 200}>
-                        Increase+
+                        disabled={count >= 200}
+                        onMouseDown={() => handleMouseDown("add")}
+                        onMouseLeave={handleMouseUp}
+                        onMouseUp={handleMouseUp}>
+                        Increase(+)
                     </button>
                 </div>
             </div>
--- vite-project/src/hooks/useSound.jsx
+++ vite-project/src/hooks/useSound.jsx
@@ -1,6 +1,8 @@
-// import {useRef} from 'react';
+import {useRef} from 'react';
 
 function useSound() {
+    const playingRef = useRef(false);
+    
     return(type) => {
         const audio = new Audio(`/sounds/${type}.mp3`);
         audio.play();
